5f1424e3f515a7df2e690137e8bc3890d31386ae
Vladislav <vlad@grokhotov.ru>
2021-06-19 15:57:12 +0300

    add validation for parsing

---
+++ ConCurrencyBot/Callbacks/Local/Parsers.swift
@@ -0,0 +1,103 @@
+//
+//  Parsers.swift
+//  ConCurrencyBot
+//
+//  Created by Vladislav Grokhotov on 19.06.2021.
+//
+
+import Foundation
+import SwiftSoup
+import SwiftyXMLParser
+
+func localParse(data: Data, currency: Currency, originalLocation: String) -> String? {
+    let doc = try! SwiftSoup.parse(String(data: data, encoding: .utf8)!)
+
+    guard
+        let offers = try? doc.select("div[class='exchange-calculator-rates table-flex__row-group']")
+            .compactMap({ element -> BankOffer? in
+                let bank = try element.select("a[class='font-bold']").first()?.text()
+                var buy = try element.select("i[class='icon-font icon-calculator-16 icon-font--size_small calculator-hover-icon']")[0].parent()?.text()
+                if let buyStr = buy {
+                    let index = buyStr.index(buyStr.startIndex, offsetBy: 7)
+                    buy = String(buyStr.prefix(upTo: index))
+                }
+                let sell = try element.select("i[class='icon-font icon-calculator-16 icon-font--size_small calculator-hover-icon']")[1].parent()?.text()
+                return BankOffer(bank: bank, buy: buy, sell: sell)
+            }),
+        offers.count > 0
+    else { return nil }
+        
+    var result = "Offers for exchanging " + currency.rawValue.uppercased() + " in " + originalLocation + ":\n"
+    
+    offers.forEach { offer in
+        result.append("\(offer.bank):\nsale for \(offer.sell)\nbuy with \(offer.buy)\n\n")
+    }
+    
+    return result
+}
+
+func localBestParse(data: Data, currency: Currency, originalLocation: String) -> String? {
+    
+    do {
+        let doc = try SwiftSoup.parse(String(data: data, encoding: .utf8)!)
+        
+        guard
+            let offerEllement = try doc.select("tr[class='currency-table__bordered-row']").first()
+        else { return nil }
+        
+        let firstContainer = try offerEllement.select("div[class='currency-table__rate__text']")
+        let secondContainer = try offerEllement.select("div[class='currency-table__large-text']")
+        
+        guard
+            firstContainer.count >= 3,
+            secondContainer.count >= 3
+        else {
+            return nil
+        }
+        
+        let bankBuy = try firstContainer[1].text()
+        let bankSell = try firstContainer[2].text()
+        let buy = try secondContainer[1].text()
+        let sell = try secondContainer[2].text()
+        
+        var result = "Best offer for exchanging " + currency.rawValue.uppercased() + " in " + originalLocation + ":\n"
+        
+        result.append("\(bankSell): sale for \(sell) ₽\n\(bankBuy): buy with \(buy) ₽\n")
+        
+        return result
+    } catch {
+        return nil
+    }
+}
+
+func cbDateParce(date: String, data: Data) -> String? {
+    
+    var resultArray = [DateCurrency]()
+    
+    let currencies = ["USD", "EUR", "GBP", "JPY", "CNY"]
+    
+    let xml = XML.parse(data)
+    for element in xml["ValCurs", "Valute"] {
+        if let currency = element["CharCode"].text, currencies.contains(currency) {
+            guard
+                let nominal = element["Nominal"].text,
+                let value =  element["Value"].text
+            else {
+                return nil
+            }
+            resultArray.append(DateCurrency(currency: currency, nominal: nominal, value: value))
+        }
+    }
+    
+    if resultArray.count == 0 {
+        return nil
+    }
+    
+    var resultString = "CB currencies rate for \(date):\n"
+    
+    resultArray.forEach { element in
+        resultString += element.nominal + " " + element.currency + " for " + element.value + " ₽\n"
+    }
+    
+    return resultString
+}
--- ConCurrencyBot/Callbacks/Local/TextCallback.swift
+++ ConCurrencyBot/Callbacks/Local/TextCallback.swift
@@ -7,8 +7,7 @@
 
 import Foundation
 import Telegrammer
-import SwiftSoup
-import SwiftyXMLParser
+
 
 ///Callback for Message handler, which send echo message to user
 func echoResponse(_ update: Update, _ context: BotContext?) throws {
@@ -21,7 +20,7 @@ func echoResponse(_ update: Update, _ context: BotContext?) throws {
         guard
             let command = userChosenCommand[user.id]
         else {
-            let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Something has happend")
+            let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Something bad has happend")
             try bot.sendMessage(params: params)
             return
         }
@@ -33,7 +32,7 @@ func echoResponse(_ update: Update, _ context: BotContext?) throws {
                 let currency = userChosenCurrency[user.id],
                 let originalLocation = message.text
             else {
-                let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Something has happend")
+                let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Something bad has happend")
                 try bot.sendMessage(params: params)
                 return
             }
@@ -42,51 +41,39 @@ func echoResponse(_ update: Update, _ context: BotContext?) throws {
             
             LocalBanksNetworkService().getCurrency(currency: currency.rawValue, location: locationForSite) { data in
         
-                let doc = try! SwiftSoup.parse(String(data: data, encoding: .utf8)!)
-        
-                let offers =
-                    try! doc.select("div[class='exchange-calculator-rates table-flex__row-group']")
-                    .compactMap({ element -> BankOffer? in
-                        let bank = try element.select("a[class='font-bold']").first()?.text()
-                        var buy = try element.select("i[class='icon-font icon-calculator-16 icon-font--size_small calculator-hover-icon']")[0].parent()?.text()
-                        if let buyStr = buy {
-                            let index = buyStr.index(buyStr.startIndex, offsetBy: 7)
-                            buy = String(buyStr.prefix(upTo: index))
-                        }
-                        let sell = try element.select("i[class='icon-font icon-calculator-16 icon-font--size_small calculator-hover-icon']")[1].parent()?.text()
-                        return BankOffer(bank: bank, buy: buy, sell: sell)
-                    })
-        
-                var result = "Offers for exchanging " + currency.rawValue.uppercased() + " in " + originalLocation + ":\n"
-        
-                offers.forEach { offer in
-                    result.append("\(offer.bank):\nsale for \(offer.sell)\nbuy with \(offer.buy)\n\n")
+                if let result = localParse(data: data, currency: currency, originalLocation: originalLocation) {
+                    
+                    let params = Bot.SendMessageParams(
+                        chatId: .chat(message.chat.id),
+                        text: result
+                    )
+                    
+                    deleteAllCaches(user.id)
+            
+                    let _ = try? bot.sendMessage(params: params)
+                } else {
+                    let params = Bot.SendMessageParams(
+                        chatId: .chat(message.chat.id),
+                        text: "No offers for \(currency.rawValue.uppercased()) in \(originalLocation)"
+                    )
+                    deleteAllCaches(user.id)
+                    let _ = try? bot.sendMessage(params: params)
                 }
-        
-                let params = Bot.SendMessageParams(
-                    chatId: .chat(message.chat.id),
-                    text: result
-                )
-                
-                userTextMode.removeValue(forKey: user.id)
-                userChosenCommand.removeValue(forKey: user.id)
-                userChosenCurrency.removeValue(forKey: user.id)
-        
-                let _ = try? bot.sendMessage(params: params)
             } errCompletion: { error in
                 let params = Bot.SendMessageParams(
                     chatId: .chat(message.chat.id),
-                    text: "Cannot get offers now"
+                    text: "Cannot get offers now. Try again or write /stop command"
                 )
                 let _ = try? bot.sendMessage(params: params)
             }
+            
         case .localBest:
             
             guard
                 let currency = userChosenCurrency[user.id],
                 let originalLocation = message.text
             else {
-                let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Something has happend")
+                let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Something bad has happend")
                 try bot.sendMessage(params: params)
                 return
             }
@@ -95,47 +82,33 @@ func echoResponse(_ update: Update, _ context: BotContext?) throws {
             
             LocalBanksNetworkService().getCurrency(currency: currency.rawValue, location: locationForSite) { data in
                 
-                let doc = try? SwiftSoup.parse(String(data: data, encoding: .utf8)!)
-                
-                let offerEllement = try? doc?.select("tr[class='currency-table__bordered-row']").first()
-                
-                let bankBuy = try? offerEllement?.select("div[class='currency-table__rate__text']")[1].text()
-                let bankSell = try? offerEllement?.select("div[class='currency-table__rate__text']")[2].text()
-                let buy = try? offerEllement?.select("div[class='currency-table__large-text']")[1].text()
-                let sell = try? offerEllement?.select("div[class='currency-table__large-text']")[2].text()
-                
-                guard
-                    let bankBuy = bankBuy, let bankSell = bankSell, let buy = buy, let sell = sell
-                else {
+                if let result = localBestParse(data: data, currency: currency, originalLocation: originalLocation) {
                     let params = Bot.SendMessageParams(
                         chatId: .chat(message.chat.id),
-                        text: "Cannot get best offer now"
+                        text: result
+                    )
+                    
+                    userTextMode.removeValue(forKey: user.id)
+                    userChosenCommand.removeValue(forKey: user.id)
+                    userChosenCurrency.removeValue(forKey: user.id)
+                    
+                    let _ = try? bot.sendMessage(params: params)
+                    
+                } else {
+                    let params = Bot.SendMessageParams(
+                        chatId: .chat(message.chat.id),
+                        text: "No offers for \(currency.rawValue.uppercased()) in \(originalLocation)"
                     )
                     let _ = try? bot.sendMessage(params: params)
-                    return
                 }
-                
-                var result = "Best offer for exchanging " + currency.rawValue.uppercased() + " in " + originalLocation + ":\n"
-                
-                result.append("\(bankSell): sale for \(sell) ₽\n\(bankBuy): buy with \(buy) ₽\n")
-                
-                let params = Bot.SendMessageParams(
-                    chatId: .chat(message.chat.id),
-                    text: result
-                )
-                
-                userTextMode.removeValue(forKey: user.id)
-                userChosenCommand.removeValue(forKey: user.id)
-                userChosenCurrency.removeValue(forKey: user.id)
-                
-                let _ = try? bot.sendMessage(params: params)
             } errCompletion: { error in
                 let params = Bot.SendMessageParams(
                     chatId: .chat(message.chat.id),
-                    text: "Cannot get best offer now"
+                    text: "Cannot get best offer now. Try again or write /stop command"
                 )
                 let _ = try? bot.sendMessage(params: params)
             }
+            
         case .cbDate:
             
             guard
@@ -143,50 +116,32 @@ func echoResponse(_ update: Update, _ context: BotContext?) throws {
                 let regex = try? NSRegularExpression(pattern: "^[0-9]{2}/[0-9]{2}/[0-9]{4}$"),
                 let _ = regex.firstMatch(in: date, options: [], range: NSRange(location: 0, length: date.count))
             else {
-                let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Wrong date, try another one")
+                let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Wrong date, try another one or write /stop command")
                 try bot.sendMessage(params: params)
                 return
             }
             
             CBNetworkService().getCurrency(date: date) { data in
                 
-                var resultArray = [DateCurrency]()
-                
-                let currencies = ["USD", "EUR", "GBP", "JPY", "CNY"]
-                
-                let xml = XML.parse(data)
-                for element in xml["ValCurs", "Valute"] {
-                    if let currency = element["CharCode"].text, currencies.contains(currency) {
-                        guard
-                            let nominal = element["Nominal"].text,
-                            let value =  element["Value"].text
-                        else {
-                            let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Cannot get currencies")
-                            let _ = try? bot.sendMessage(params: params)
-                            return
-                        }
-                        resultArray.append(DateCurrency(currency: currency, nominal: nominal, value: value))
-                    }
+                if let result = cbDateParce(date: date, data: data) {
+                    userTextMode.removeValue(forKey: user.id)
+                    userChosenCommand.removeValue(forKey: user.id)
+                    
+                    let params = Bot.SendMessageParams(
+                        chatId: .chat(message.chat.id),
+                        text: result
+                    )
+                    let _ = try? bot.sendMessage(params: params)
+                } else {
+                    let params = Bot.SendMessageParams(chatId: .chat(message.chat.id), text: "Cannot get currencies for this date. Try another one or write /stop command")
+                    let _ = try? bot.sendMessage(params: params)
                 }
                 
-                var resultString = "CB currencies rate for \(date):\n"
                 
-                resultArray.forEach { element in
-                    resultString += element.nominal + " " + element.currency + " for " + element.value + " ₽\n"
-                }
-                
-                userTextMode.removeValue(forKey: user.id)
-                userChosenCommand.removeValue(forKey: user.id)
-                
-                let params = Bot.SendMessageParams(
-                    chatId: .chat(message.chat.id),
-                    text: resultString
-                )
-                let _ = try? bot.sendMessage(params: params)
             } errCompletion: { error in
                 let params = Bot.SendMessageParams(
                     chatId: .chat(message.chat.id),
-                    text: "Cannot get currencies"
+                    text: "Cannot get currencies now. Try again or write /stop command"
                 )
                 let _ = try? bot.sendMessage(params: params)
             }
