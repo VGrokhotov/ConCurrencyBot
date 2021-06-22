5bcbf687f37fcb089277ec752ab6bfbc9a001a38
Vladislav Grokhotov <50099240+VGrokhotov@users.noreply.github.com>
2021-06-22 17:04:32 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,15 +9,11 @@ let package = Package(
             .macOS(.v10_14),
         ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.3.2"),
         .package(url: "https://github.com/yahoojapan/SwiftyXMLParser.git", from: "5.3.0"),
         .package(url: "https://github.com/givip/Telegrammer", from: "1.0.0-alpha.1"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "ConCurrencyBot",
             dependencies: ["SwiftSoup", "SwiftyXMLParser", "Telegrammer"]),
